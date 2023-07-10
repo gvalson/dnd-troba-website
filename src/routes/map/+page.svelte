72cb94060c84ceace40f9de9c03f20fd0846ec7a
gvalson <arsidze@gmail.com>
2023-07-10 21:45:27 +0400

    Add map image to the map page

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -2,6 +2,27 @@
     <h1>რუკა</h1>
 </div>
 
-<p>
-    რუკა დაიდება აქ.
-</p>
+<div class="map-image">
+    <img alt="ტრობას რუკა" src="/img/map-v2.jpg"/>
+    <a href="/img/map-v2.jpg">იხილეთ სრული ვერსია</a>
+</div>
+
+<style>
+ .map-image {
+     display: flex;
+     flex-direction: column;
+     max-width: 50%;
+ }
+ img {
+     border-radius: 4px;
+     margin-bottom: 5px;
+ }
+ a {
+     color: inherit;
+ }
+ @media screen and (max-width:420px) {
+     .map-image {
+	 max-width: 100%;
+     }
+ }
+</style>
