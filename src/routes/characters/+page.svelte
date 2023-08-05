e4bb0f8f8b51f7813c382c6c1ee77a0b9ff547f6
gvalson <arsidze@gmail.com>
2023-08-05 14:14:47 +0400

    Add character cards

--- src/routes/characters/+page.svelte
+++ src/routes/characters/+page.svelte
@@ -1,5 +1,79 @@
+<script>
+ import CharacterCard from './CharacterCard.svelte';
+
+ let chars = [
+     {
+	 name: "ნერმან ფონ ვულდრა",
+	 class_ka: "მეიარაღე",
+	 class_en: "Gunslinger",
+	 short_introduction: "„მოსიარულე იარაღის არსენალად“ ცნობილი ტონეთელი ადამიანი, რომელსაც უჩვეულო საქმეები აკავშირებს ყველასთან.",
+	 description: "",
+	 status: "☀️"
+     },
+     {
+	 name: "უგლუკ ზრეთნი",
+	 class_ka: "გრძნეული",
+	 class_en: "Sorcerer",
+	 short_introduction: "ზონდთა კავშირიდან დახსნილი, იდუმალი მაგიური უნარების მქონე და სისხლის უჩვეულოდ მოყვარული ნახევარორკი.",
+	 description: "",
+	 status: "☀️"
+     },
+     {
+	 name: "სალვადორ „ლამაგიუს“ დელაგარი",
+	 class_ka: "ჯადოქარი",
+	 class_en: "Wizard",
+	 short_introduction: "მუდამ ხელმალული და მანტიაში მოსილი ადამიანი, რომელსაც ანტიკორუფციული კორპორაცია აქტიურად ეძებს.",
+	 description: "",
+	 status: "☀️"
+     },
+     {
+	 name: "ადამ აურერიელი",
+	 class_ka: "ბარბაროსი",
+	 class_en: "Barbarian",
+	 short_introduction: "იდილიის გამორჩეული ოჯახის წევრი. უჩვეულოდ განმარტოებული და მშვიდი. სალვადორი მისთვის დაკარგული ძმაა. ყოველთვის იძახის: „მე მტრები არ მყავს“.",
+	 description: "",
+	 status: "☀️"
+     },
+     {
+	 name: "ბალტო ივნისი",
+	 class_ka: "მოციქული",
+	 class_en: "Cleric",
+	 short_introduction: "სხვებისთვის უცნობი მიზნით ჩამოსული ადამიანი. დამკვირვებელის როლს თამაშობს საწყის ეტაპზე.",
+	 description: "",
+	 status: "☀️"
+     },
+ ]
+</script>
+
 <div class="preamble">
     <h1>პერსონაჟები</h1>
 </div>
 
-<p>ისინი ჯერ არ არსებობენ...</p>
+<div id="chars-grid">
+    <CharacterCard {...chars[0]}/>
+    <CharacterCard {...chars[1]}/>
+    <CharacterCard {...chars[2]}/>
+    <CharacterCard {...chars[3]}/>
+    <CharacterCard {...chars[4]}/>
+</div>
+
+<style>
+ #content {
+     width: 50%;
+ }
+ #chars-grid {
+     /* background-color: #FEF4EC;
+	border: solid 2px #261302; */
+     display: grid;
+     grid-template-columns: 1fr 1fr;
+ }
+ @media screen and (max-width:900px) {
+     #content {
+	 width: inherit;
+     }
+     #chars-grid {
+	 display: flex;
+	 flex-direction: column;
+     }
+ }
+</style>
---
+++ src/routes/characters/CharacterCard.svelte
@@ -0,0 +1,50 @@
+<script>
+ export let name = "უსახელო"
+ export let class_ka;
+ export let class_en;
+ export let short_introduction;
+ export let description;
+ export let status;
+</script>
+
+<div class="char-card">
+    <div class="ns">
+	<span class="char-status">{status}</span>
+	<div class="name">{name}</div>
+    </div>
+    <div class="char-class"><em>{class_ka}</em></div>
+    <div class="intro">{short_introduction}</div>
+</div>
+
+<style>
+ .char-card {
+     background-color: #261302;
+     color: #FEF4EC;
+     margin: 5px;
+     border: 10px double #FEF4EC;
+     border-radius: 10px;
+     padding: 15px;
+ }
+ .char-card > div {
+     margin: 5px;
+ }
+ .ns {
+     display: flex;
+     flex-direction: column-reverse;
+ }
+ .char-status {
+     position: absolute;
+     text-align: center;
+     height: inherit;
+ }
+ .name {
+     text-align: center;
+     font-weight: bold;
+     font-size: 1.5em;
+     /* display: inline-block; */
+ }
+ .char-class {
+     text-align: center;
+     font-size: 1.25em;
+ }
+</style>
